27b1553ec8c7573f0af4d82ceeb20a435b7daf97
Daniel Malone <danieljmalone@gmail.com>
2017-05-06 22:42:39 -0500

    refactor code

--- src/com/finepointstudios/Main.java
+++ src/com/finepointstudios/Main.java
@@ -2,16 +2,25 @@ package com.finepointstudios;
 
 public class Main {
 
-    private static String word = "aa";
+    private static String word = "DanielA";
     private static boolean[] list = new boolean[128];
 
     public static void main(String[] args) {
+        if (search()) {
+            System.out.println("Duplicates found!");
+        } else {
+            System.out.println("No duplicates found!");
+        }
+    }
+
+    private static boolean search() {
         for (int i = 0; i < word.length(); i++) {
             char value = word.charAt(i);
             if (list[value]) {
-                System.out.println("Duplicate value found!");
+                return true;
             }
             list[value] = true;
         }
+        return false;
     }
 }
